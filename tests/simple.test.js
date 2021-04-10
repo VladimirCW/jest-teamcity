6aa2cbb16bd4e9f41a7b263bd6cea3464f849f81
vova.dmytriukhin <volodymyr.dmytriukhin@dev.pro>
2021-04-10 11:36:29 +0300

    added tests

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -1,9 +1,17 @@
 describe('Root Suite', () => {
   it('Root IT', () => {
     console.log('root IT stdout');
+    console.log("##teamcity[testSuiteStarted name='suiteName']");
+    console.log("##teamcity[testSuiteStarted name='nestedSuiteName']");
+    console.log("##teamcity[testStarted name='package_or_namespace.ClassName.TestName']");
+
     console.log("##teamcity[testMetadata testName='package.Test.testName' name='setUp time' type='number' value='434.5']");
     console.log("##teamcity[testMetadata testName='package.Test.testName' name='some key' value='a text']");
     console.log("##teamcity[testMetadata testName='test.name' name='JetBrains' type='link' value='https://jetbrains.com']");
+
+    console.log("##teamcity[testFinished name='package_or_namespace.ClassName.TestName']");
+    console.log("##teamcity[testSuiteFinished name='nestedSuiteName']");
+    console.log("##teamcity[testSuiteFinished name='suiteName']");
     expect(true).toEqual(false);
   });
 })
