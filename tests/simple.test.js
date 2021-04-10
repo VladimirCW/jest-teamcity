213e26e64389e6be2f90eb2a2cc6732054deb8c7
vova.dmytriukhin <volodymyr.dmytriukhin@dev.pro>
2021-04-10 11:27:14 +0300

    added tests

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -1,6 +1,9 @@
 describe('Root Suite', () => {
   it('Root IT', () => {
     console.log('root IT stdout');
+    console.log("##teamcity[testMetadata testName='package.Test.testName' name='setUp time' type='number' value='434.5']");
+    console.log("##teamcity[testMetadata testName='package.Test.testName' name='some key' value='a text']");
+    console.log("##teamcity[testMetadata testName='test.name' name='JetBrains' type='link' value='https://jetbrains.com']");
     expect(true).toEqual(false);
   });
 })
